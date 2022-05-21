bd104a780bc88a5a39104987fb8eca070da6f86a
andressafv9 <101218624+andressafv9@users.noreply.github.com>
2022-05-21 17:53:49 -0300

    refactor: write on file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 const { SerialPort } = require('serialport')
+const fs = require('fs');
 const { ReadlineParser } = require('@serialport/parser-readline')
 const port = new SerialPort({ path: 'COM5', baudRate: 9600 })
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 parser.on('data', (serialData) => {
-    console.log(serialData);
+    const dataJson = serialData + '\n';
+    fs.appendFile('./serialPortData.txt', dataJson, (error) => {
+        if(error) console.log(error);
+        console.log(dataJson);
+        console.log('sucessfully written to file.');
+    });
 });
