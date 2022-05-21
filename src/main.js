b2a1285cd007f8047cd62c4f677e9216c0eda8d0
andressafv9 <101218624+andressafv9@users.noreply.github.com>
2022-05-21 17:53:29 -0300

    feat: write on file

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ const { ReadlineParser } = require('@serialport/parser-readline')
 const port = new SerialPort({ path: 'COM5', baudRate: 9600 })
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
-parser.on('data', console.log)
+parser.on('data', (serialData) => {
+    console.log(serialData);
+});
