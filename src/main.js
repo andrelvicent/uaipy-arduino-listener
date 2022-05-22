6cbf4c47d83153fb0690715f853bde2da3cee6de
andressafv9 <101218624+andressafv9@users.noreply.github.com>
2022-05-21 21:34:50 -0300

    refactor: add to storage

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
-const { SerialPort } = require('serialport')
+const { SerialPort } = require('serialport');
 const fs = require('fs');
-const { ReadlineParser } = require('@serialport/parser-readline')
-const port = new SerialPort({ path: 'COM5', baudRate: 9600 })
+const { ReadlineParser } = require('@serialport/parser-readline');
+const port = new SerialPort({ path: 'COM5', baudRate: 9600 });
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+const storageName = './serialPortData.txt';
 
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
-parser.on('data', (serialData) => {
-    const dataJson = serialData + '\n';
-    fs.appendFile('./serialPortData.txt', dataJson, (error) => {
-        if(error) console.log(error);
-        console.log(dataJson);
-        console.log('sucessfully written to file.');
-    });
+parser.on('data', (arduinoSerialPortData) => {
+    const newRecord = adaptToStorage(arduinoSerialPortData);
+    addToStorage(newRecord);
+    print(newRecord);
 });
+
+function adaptToStorage (arduinoSerialPortData) { 
+    const dataParser = JSON.parse(arduinoSerialPortData);
+    let clock = new Date();
+    dataParser.dia = "" + clock.getDate() + "/" + (clock.getMonth()+1) + "/" + clock.getFullYear();
+    dataParser.horario = "" + clock.getHours() + ":" + clock.getMinutes() + ":" + clock.getMilliseconds();
+    return JSON.stringify(dataParser) + ",";
+}
+
+function print(data) { 
+    console.log("--------------------------");
+    console.log(data);
+    console.log("--------------------------");
+}
+
+function addToStorage (newRecord) {
+    fs.appendFile(storageName, newRecord, (error) => {
+        if(error) print(error);
+    });
+}
