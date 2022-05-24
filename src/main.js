e0528b172b1de5346a7daa65753367b0d905d5c7
André Luiz Vicente <andrevicente.ufu@gmail.com>
2022-05-23 21:55:32 -0300

    refactor: rename env vars

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 const { SerialPort } = require('serialport');
 const fs = require('fs');
 const { ReadlineParser } = require('@serialport/parser-readline');
-const port = new SerialPort({ path: 'COM5', baudRate: 9600 });
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
-const storageName = './serialPortData.txt';
 
-parser.on('data', (arduinoSerialPortData) => {
+const storageName = './storage/dataStorage.txt';
+const pathToSerialPort = 'COM5';
+const serialPortBaudRate = 9600;
+const delimiterType = '\r\n';
+
+const serial = new SerialPort({ path: pathToSerialPort, baudRate: serialPortBaudRate });
+const serialParser = serial.pipe(new ReadlineParser({ delimiter: delimiterType }));
+
+serialParser.on('data', (arduinoSerialPortData) => {
     const newRecord = adaptToStorage(arduinoSerialPortData);
     addToStorage(newRecord);
     print(newRecord);
@@ -14,7 +19,7 @@ parser.on('data', (arduinoSerialPortData) => {
 function adaptToStorage (arduinoSerialPortData) { 
     const dataParser = JSON.parse(arduinoSerialPortData);
     let clock = new Date();
-    dataParser.dia = "" + clock.getDate() + "/" + (clock.getMonth()+1) + "/" + clock.getFullYear();
+    dataParser.dia = "" + clock.getDate() + "/" + (clock.getMonth()+1);
     dataParser.horario = "" + clock.getHours() + ":" + clock.getMinutes() + ":" + clock.getMilliseconds();
     return JSON.stringify(dataParser) + ",";
 }
